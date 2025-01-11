d7a2d703ddca03b70813b42d56727e3820464110
Emmaowilli <emmadicksonowilli2022@gmail.com>
2025-01-11 21:15:54 +0300

    Finish week 3,4 and 5

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Console.WriteLine("Welcome to the Guess My Number game!");
+
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int attempts = 0;
+
+            int guess = -1;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    guess = int.Parse(input);
+                    attempts++; 
+
+                    if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed it! The magic number was {magicNumber}.");
+                        Console.WriteLine($"It took you {attempts} attempts.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+
+            if (response != "yes")
+            {
+                playAgain = false;
+                Console.WriteLine("Thanks for playing!");
+            }
+        }
     }
 }
